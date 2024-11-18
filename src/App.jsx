41455249083d00a93813c2746d7852203a4e78e7
Niklas Röttger <niklas.roettger@outlook.de>
2024-11-18 10:26:13 +0100

    add session code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
 export default function App() {
-  return <div>Add JSX here</div>;
+  return (
+    <div>
+      <Button />
+      <Button />
+      <Button />
+    </div>
+  );
+}
+
+function Button() {
+  const buttonText = "click with React";
+
+  return (
+    <button
+      className="button highlight"
+      type="button"
+      onClick={() => console.log("Hello React World")}
+    >
+      {buttonText}
+    </button>
+  );
 }
--- src/VanillaApp.js
+++ src/VanillaApp.js
@@ -1,5 +1,25 @@
 export default function VanillaApp() {
   const div = document.createElement("div");
   div.innerHTML = "Vanilla JS";
+
+  const buttonElement = Button();
+  const buttonElement2 = Button();
+  const buttonElement3 = Button();
+
+  div.append(buttonElement);
+  div.append(buttonElement2);
+  div.append(buttonElement3);
+
   return div;
+}
+
+function Button() {
+  const buttonElement = document.createElement("button");
+  buttonElement.textContent = "click me";
+  buttonElement.type = "button";
+  buttonElement.addEventListener("click", () => {
+    console.log("Hello World");
+  });
+
+  return buttonElement;
 }
